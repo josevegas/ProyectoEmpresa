7a664466e44d08f9cd999cd382b8d1c6f9f36ab5
JOSELUIS <josevegas.marquez@gmail.com>
2025-01-17 00:48:39 -0500

    implementando carrusel

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  wddescriptionhead='Si deseas ser parte del mundo digital, te podemos brindar los siguientes productos'
+  wdservicios=['Diseño de Páginaa Web','Aplicación E-Commerce','Catálogo Digital']
 }
