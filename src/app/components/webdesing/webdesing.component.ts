da912768e5258e538aa7d8a8eab1fab34c1eac2b
joseluisvegas <161845982+joseluisvegas@users.noreply.github.com>
2025-02-03 23:26:54 -0500

    diseño web

--- src/app/components/webdesing/webdesing.component.ts
+++ src/app/components/webdesing/webdesing.component.ts
@@ -8,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './webdesing.component.css'
 })
 export class WebdesingComponent {
-
+cabecera:string='';
+ecommerse:string='';
+catalogo:string='';
+web:string='';
+landing:string='';
+private initializeCabecera():void{
+  this.cabecera+='<b>Transformamos ideas en experiencias digitales irresistibles</b>. No solo creamos sitios web, construimos puentes entre tu marca y tu audiencia:<br />';
+this.cabecera+='<ul><li>Interfaces fluídas</li>';
+this.cabecera+='<li>Diseños responsivos</li>';
+this.cabecera+='<li>Funcionalidades inteligentes que convierten visitantes en aliados</li></ul>'
+this.cabecera+='Ten una web que no solo se vea increible, sino que vende, comunica y seduce. Porque en la era digital, tu sitio web es tu mejor tarjeta de presentación <b>¡Y aquí estamos para hacerla brillar como nunca!</b>';
+}
+ngOnInit():void{
+  this.initializeCabecera();
+}
 }
